2e37997160e633514c8c5b02e9a6de6dcbe77d09
imandal98 <indrajeetmandal.aaa@gmail.com>
2023-12-13 21:24:17 +0530

    v2

--- client/src/Sections/Header/Search/Search.jsx
+++ client/src/Sections/Header/Search/Search.jsx
@@ -1,5 +1,32 @@
-const Search =() => {
-    return <div> Search</div>;
+import { IoCloseCircleSharp } from "react-icons/io5";
+import "./../Search/Search.scss";
+import item_img from "../../../images/product/mcb.png";
+const Search = ({ setShowSearch }) => {
+  return (
+    // <div className="search-bar">
+    //   <div className="form-data">
+    //     {/* <input type="text" autoFocus placeholder="Search" /> */}
+    //     {/* <IoCloseCircleSharp onClick={() => setShowSearch(false)} /> */}
+    //   </div>
+    <div className="search-result-container">
+      <div className="search-result">
+        <span className="close" onClick={() => setShowSearch(false)}>
+          <IoCloseCircleSharp size={18} />
+          <span className="txt">close</span>
+        </span>
+        <div className="search-result-items">
+          <div className="item_img">
+            <img src={item_img} alt="" />
+          </div>
+          <div className="item_details">
+            <div className="name">Product Name</div>
+            <div className="description">Product Description</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
 };
 
 export default Search;
--- client/src/contex/contex.js
+++ client/src/contex/contex.js
@@ -1,9 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 export const Context = createContext();
 
 const Appcontxt = ({ children }) => {
   const [categories, setcategories] = useState();
   const [products, setProducts] = useState();
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  const [cartSubtotal, setCartSubtotal] = useState(0);
+  const loaction = useLocation();
+
+  useEffect(() => {}, [cartItems]);
+  const handleAddToCart = (product, quantity) => {};
+  const handleRemoveFromCart = (product) => {};
+  const handleCartProductQuantity = (type, product) => {};
+
   return (
     <Context.Provider
       value={{
@@ -11,6 +22,15 @@ const Appcontxt = ({ children }) => {
         setcategories,
         products,
         setProducts,
+        cartItems,
+        setCartItems,
+        cartCount,
+        setCartCount,
+        cartSubtotal,
+        setCartSubtotal,
+        handleAddToCart,
+        handleRemoveFromCart,
+        handleCartProductQuantity,
       }}
     >
       {" "}
